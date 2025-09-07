898e4fc3074a3518dbaf5f44572a25b8699ca2bc
Adesina <adesinaferanmi46@gmail.com>
2025-09-07 15:38:14 +0100

    Applied inline CSS styling to Header, MainContent, UserProfile, and Footer components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: 'black', color: 'white', padding: '15px', textAlign: 'center' }}>
+      <p>© 2025 My React App. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#eef2f3', textAlign: 'center' }}>
+      <p>Welcome to my React app. Here are some cities I love!</p>
+    </main>
+  );
+}
+
+export default MainContent;
